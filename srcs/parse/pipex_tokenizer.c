3fa3da344b6000f8e58269a21040fd03ab497f37
htoe <htoe@student.42bangkok.com>
2026-02-16 18:21:06 +0700

    pipex: parsing cmd_tokenizer for each string

--- srcs/parse/pipex_tokenizer.c
+++ srcs/parse/pipex_tokenizer.c
@@ -6,7 +6,7 @@
 /*   By: htoe <htoe@student.42bangkok.com>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/16 16:58:45 by htoe              #+#    #+#             */
-/*   Updated: 2026/02/16 17:43:54 by htoe             ###   ########.fr       */
+/*   Updated: 2026/02/16 18:20:32 by htoe             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,11 @@ t_parse_case	count_token(char *str, t_token *token)
 			token->in_word = 0;
 		else
 		{
-			if (!token->in_word && ++token->count)
+			if (!token->in_word)
+			{
+				token->count++;
 				token->in_word = 1;
+			}
 			if (str[token->i] == '\'')
 				token->in_sq = !token->in_sq;
 			else if (str[token->i] == '\\' && !token->in_sq)
@@ -51,14 +54,70 @@ int	token_counter(char *str, t_parse_case *state)
 	return (token.count);
 }
 
+void	free_array(char ***arr)
+{
+	int	i;
+
+	if (!*arr)
+		return ;
+	i = 0;
+	while ((*arr)[i])
+		free((*arr)[i++]);
+	free(*arr);
+	*arr = NULL;
+}
+
+char	*build_cmd_argv(char *s, int *i, t_parse_case *state)
+{
+	char	*buffer;
+	t_token	token;
+
+	while (s[*i] == ' ')
+		(*i)++;
+	buffer = (char *)malloc(sizeof(char) * (ft_strlen(s) - *i + 1));
+	if (!buffer)
+		return (*state = PARSE_MALLOC_FAIL, NULL);
+	token.i = 0;
+	token.in_sq = 0;
+	while (s[*i] && (token.in_sq || s[*i] != ' '))
+	{
+		if (s[*i] == '\'')
+			token.in_sq = !token.in_sq;
+		else if (s[*i] == '\\' && !token.in_sq)
+		{
+			(*i)++;
+			buffer[token.i++] = s[*i];
+		}
+		else
+			buffer[token.i++] = s[*i];
+		(*i)++;
+	}
+	buffer[token.i] = '\0';
+	return (buffer);
+}
+
 char	**cmd_tokenizer(char *cmd_str)
 {
 	char			**argv;
 	int				token_count;
 	t_parse_case	state;
+	int				i;
+	int				index;
 
 	token_count = token_counter(cmd_str, &state);
-	printf("token count: %d\n", token_count);
-	argv = NULL;
+	if (state != PARSE_OK)
+		return (NULL);
+	argv = (char **)malloc(sizeof(char *) * (token_count + 1));
+	if (!argv)
+		return (NULL);
+	index = 0;
+	i = 0;
+	while (i < token_count)
+	{
+		argv[i++] = build_cmd_argv(cmd_str, &index, &state);
+		if (state != PARSE_OK)
+			return (free_array(&argv), NULL);
+	}
+	argv[i] = NULL;
 	return (argv);
 }
